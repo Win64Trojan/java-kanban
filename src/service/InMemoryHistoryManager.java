eb16b7cb4254254f9a8b977c02918785c5d3576b
Aleksandr Ivanov <sasha942710@yandex.ru>
2024-05-30 21:12:14 +0300

    Исправить код, под checkStyle

--- src/service/InMemoryHistoryManager.java
+++ src/service/InMemoryHistoryManager.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 public class InMemoryHistoryManager implements HistoryManager {
     private Node<Task> head = null;
